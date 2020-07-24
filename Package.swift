64637aca5fdeadb8c52587202c63af75f9a76d89
Meseery <eng.m.elmeseery@gmail.com>
2020-07-25 01:58:17 +0200

    Update package specs.

--- Package.swift
+++ Package.swift
@@ -23,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "APILayer",
-            dependencies: []),
-        .testTarget(
-            name: "APILayerTests",
-            dependencies: ["APILayer"]),
+            dependencies: [])
     ]
 )
